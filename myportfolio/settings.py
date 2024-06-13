16622a2480c7137b12a20bcfc40995e140e6d99a
Shashank Agasimani <115537298+styloo007@users.noreply.github.com>
2024-06-13 12:09:02 +0530

    Update settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tdbtx=tw1q8t66g3x$9*muh9n4t*-imn!aeeg_+lebb&6-062)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
